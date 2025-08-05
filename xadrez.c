03a8695370017a0e8ff758231b2db32bcd944098
jeb422 <hellboy_knight@hotmail.com>
2025-08-05 03:04:18 +0000

    novo commit

--- xadrez.c
+++ xadrez.c
@@ -25,5 +25,21 @@ int main() {
         rainha++;
     } while (rainha <= 5);
 
+    printf("\n");
+    
+    int i;
+    for (i = 0; i < 1; i++) {  
+        int j = 0;
+
+        // Primeiro: duas casas para baixo 
+        while (j < 2) {
+            printf("Cavalo moveu para Baixo\n");
+            j++;
+        }
+
+        // Depois: uma casa para a esquerda
+        printf("Cavalo moveu para Esquerda\n");
+    }
+
     return 0;
 }
